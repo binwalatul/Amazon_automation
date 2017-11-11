c89baea61025adb697fc4070b64cd37bd1840545
Atul Binwal <abinwal@shutterstock.com>
2017-11-11 22:05:33 +0530

    first commit

--- src/main/java/common/driver_factory.java
+++ src/main/java/common/driver_factory.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class driver_factory {
     public static void browser()
     {
-        System.setProperty("webdriver.chrome.driver", "//home//atul//IdeaProjects//Selenium_test//src//drivers//chromedriver");
+        System.setProperty("webdriver.chrome.driver", "//home//atul//drivers//chromedriver");
 
     }
 }
