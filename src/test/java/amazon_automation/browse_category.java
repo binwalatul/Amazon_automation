fc00a29736890989d81f01b677615c15e537059a
Atul Binwal <abinwal@shutterstock.com>
2017-11-12 18:31:37 +0530

    THird commit

--- src/test/java/amazon_automation/browse_category.java
+++ src/test/java/amazon_automation/browse_category.java
@@ -1,14 +1,23 @@
 package amazon_automation;
 
 import common.driver_factory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import javax.swing.*;
 
 public class browse_category extends driver_factory {
     public static void main(String[] args){
-        String URL="amazon.in";
+        String URL="https://www.amazon.in/";
         browser();
         ChromeDriver driver = new ChromeDriver();
         driver.get(URL);
+        WebElement element=driver.findElementByCssSelector("#nav-shop .nav-line-2");
+//        WebElement element =driver.findElementByCssSelector(".nav-template.nav-flyout-content.nav-tpl-itemList span:nth-child(6)");
+        Actions action= new Actions(driver);
+        action.moveToElement(element).moveToElement(driver.findElement(By.cssSelector(".nav-template.nav-flyout-content.nav-tpl-itemList span:nth-child(6)"))).click().build().perform();
 
+        }
     }
-}
